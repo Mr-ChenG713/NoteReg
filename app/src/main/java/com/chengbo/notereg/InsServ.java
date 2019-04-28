e7b9452a42d77fca57df56288e91dedb53ef08cc
Mr-ChenG713 <48352365+Mr-ChenG713@users.noreply.github.com>
2019-04-29 00:43:55 +0100

    WindSer and WindTy feitos

--- app/src/main/java/com/chengbo/notereg/InsServ.java
+++ app/src/main/java/com/chengbo/notereg/InsServ.java
@@ -9,6 +9,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class InsServ extends AppCompatActivity {
 
@@ -19,6 +21,30 @@ public class InsServ extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+    }
+
+    public void Save_Button(View view) {
+
+        EditText editTextServ = (EditText) findViewById(R.id.InsServ);
+
+        String message = editTextServ.getText().toString();
+
+        if (message.trim().length() == 0){
+
+            editTextServ.setError(getString(R.string.title_activity_ins_serv));
+            editTextServ.requestFocus();
+            return;
+
+        }
+
+        Toast.makeText(this,(R.string.State_s_Inser), Toast.LENGTH_SHORT).show();
+
+        finish();
+    }
+
+    public void Cancel_Button (View view){
+
+        finish();
     }
 
 }
--- app/src/main/java/com/chengbo/notereg/WindTy.java
+++ app/src/main/java/com/chengbo/notereg/WindTy.java
@@ -1,5 +1,6 @@
 package com.chengbo.notereg;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Toast;
 
 public class WindTy extends AppCompatActivity {
 
@@ -19,7 +21,27 @@ public class WindTy extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+    }
 
+    public  void  InsTp (View view){
+
+        Intent intent = new Intent(this, InsTy.class);
+        Toast.makeText(this, (R.string.title_activity_ins_ty), Toast.LENGTH_SHORT).show();
+        startActivity(intent);
+    }
+
+    public  void  EDTp (View view){
+        // Intent intent = new Intent(this, EditTyp.class);
+
+        //Toast.makeText(this,(R.string.WindTypes_RegTypes_noti_edi), Toast.LENGTH_SHORT).show();
+
+        //startActivity(intent);
+    }
+
+
+    public void Cancel_Button (View view){
+
+        finish();
     }
 
 }
